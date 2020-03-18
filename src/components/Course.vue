8a6afdfd6e77817fa3b79fb64e2343bcd5d12e30
Ryan Freeman <N00173767@student.iadt.ie>
2020-03-18 19:57:51 +0000

    Updated course component

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -1,12 +1,11 @@
 <template>
-    <tr>
+    <router-link :to="{name: 'viewCourse', params: {id: course.id}}" tag="tr"
+                 class="cursor-pointer">
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
             <div class="flex items-center">
                 <div>
-                    <div class="text-sm leading-5 font-medium text-gray-900 hover:underline">
-                        <router-link
-                                :to="{name: 'viewCourse', params: {id: course.id}}">{{ course.title }}
-                        </router-link>
+                    <div class="text-sm leading-5 font-medium text-gray-900">
+                        {{ course.title }}
                     </div>
                 </div>
             </div>
@@ -21,17 +20,17 @@
             </div>
         </td>
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-            <div class="text-sm leading-5 text-gray-500">
+            <div class="text-sm leading-5 font-medium text-gray-900">
                 {{ course.description }}
             </div>
         </td>
-        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
             {{ course.points }}
         </td>
-        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
             {{ course.level }}
         </td>
-    </tr>
+    </router-link>
 </template>
 
 <script>
