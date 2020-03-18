9375c7f2a5b73d1a7b18f6ecbfc65fea7790663c
Ryan Freeman <N00173767@student.iadt.ie>
2020-03-18 20:04:18 +0000

    Updated course view

--- src/views/courses/Course.vue
+++ src/views/courses/Course.vue
@@ -22,7 +22,7 @@
                     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
                         <div class="flex justify-between items-center px-4 py-5 border-b border-gray-200 sm:px-6">
                             <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                {{ course.title }}
+                                Course
                             </h3>
                             <div>
                                 <button type="button"
