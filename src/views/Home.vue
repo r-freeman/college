4850746d0c7ab22191c539781b11852e59af1004
Ryan Freeman <N00173767@student.iadt.ie>
2020-03-23 13:11:06 +0000

    Updated home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="w-full h-screen bg-whitesmoke">
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <div class="px-4 py-6 sm:px-0">
+    <div>
+        <header class="bg-white border-b-2 border-gray-200">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 h-18">
+                <div class="flex justify-between items-center">
+                    <h2 class="text-lg font-semibold leading-tight text-gray-900">
+                        Dashboard
+                    </h2>
+                </div>
+            </div>
+        </header>
+        <div class="w-full h-screen bg-whitesmoke">
+            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div class="px-4 py-6 sm:px-0">
 
+                </div>
             </div>
         </div>
     </div>
